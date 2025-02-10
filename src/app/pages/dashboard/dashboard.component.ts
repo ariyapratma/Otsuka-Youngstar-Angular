e309a9922752ea4680ccc4ac36d9424ec920a6ce
Ariya <apanpratama1103@gmail.com>
2025-02-10 14:57:16 +0700

    Consist Style general in Home, Master User, Dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiServiceService } from '../../services/api-service.service';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexYAxis,
+} from 'ng-apexcharts';
+
+export interface User {
+  id: number;
+  title: string;
+  category_id: number;
+  description: string;
+  progress: number;
+}
+
+export interface ApexchartInterface {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  dataLabels: ApexDataLabels;
+  yaxis: ApexYAxis;
+}
 
 @Component({
   selector: 'app-dashboard',
   standalone: false,
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  userData: User[] = [];
+  chartOptions!: ApexchartInterface;
 
+  constructor(private restApiService: ApiServiceService) {}
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  private getEmployees() {
+    this.restApiService.getUser().subscribe(
+      (data) => {
+        this.userData = data.data;
+        this.showChart();
+      },
+      (error) => {
+        console.error('Error:', error);
+      }
+    );
+  }
+
+  private showChart() {
+    this.chartOptions = {
+      series: [
+        {
+          name: 'User Progress',
+          data: this.userData.map((user) => user.progress),
+        },
+      ],
+      chart: {
+        type: 'line',
+        height: 350,
+      },
+      xaxis: {
+        categories: this.userData.map((user) => user.title),
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      yaxis: {
+        title: {
+          text: 'User Progress (%)',
+        },
+      },
+    };
+  }
 }
